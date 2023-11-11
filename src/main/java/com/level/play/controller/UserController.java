253c63d620524cb8f5284ed18b2b0f30801c650c
f4925246 <f4925246@fnb.co.za>
2023-11-11 13:18:51 +0200

    Fix table creation errors

--- src/main/java/com/level/play/controller/UserController.java
+++ src/main/java/com/level/play/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/levelplay/user")
 public class UserController {
 
     private final UserService userService;
